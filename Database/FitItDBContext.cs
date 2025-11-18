844dc346051c90c17c7bcf4e1adf4e66d16ba0b9
Adan-Alvarado <alvaradoadan55@gmail.com>
2025-11-18 17:44:31 -0600

    Corrigiendo datos BD

--- Database/FitItDBContext.cs
+++ Database/FitItDBContext.cs
@@ -1,31 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using fixIt.Api.Entities;
 
-namespace fixIt.Api.Database
+public class FixItDbContext : DbContext
 {
-    public class FitItDBContext
+    public FixItDbContext(DbContextOptions<FixItDbContext> options)
+        : base(options)
     {
-        [Column("id")]
-        public Guid Id { get; set; }
-
-        //Auditoia
-        [Column("titulo")]
-        public string Titulo { get; set; }
-
-        [Column("descripcion")]
-        public string descripcion { get; set; }
-
-        [Column("categoria_id")]
-        public string CategoriaId { get; set; }   
-
-        [Column("usuario_id")]
-        public string UsuarioId { get; set; } 
-        
-        [Column("fecha_creacion")]
-        public DateTime FechaCreacion { get; set; }
-       
-        [Column("fecha_creacion")]
-        public string EstadoReporte { get; set; }
-        
-        [Column("url_multimedia")]
-        public string URLMultimedia { get; set; }
     }
+
+    public DbSet<Reporte> Reportes { get; set; }
+    public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<Categoria> Categorias { get; set; }
 }

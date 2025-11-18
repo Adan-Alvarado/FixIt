3d76ab267aa89d401d75bd2dcd286469603edbb9
Joseph Carias <cariasjoseph11@gmail.com>
2025-11-18 10:19:36 -0600

    Create ReporteEntityand clean Program.cs

---
+++ Entities/ReporteEntity.cs
@@ -0,0 +1,18 @@
+
+namespace FixIt.Entities
+{
+    
+
+    public class ReporteEntity
+    {
+        public Guid Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public Guid CategoriaId { get; set; } // 
+        public Guid UsuarioId { get; set; } // quien reporta
+        public DateTime FechaCreacion { get; set; }
+        public DateTime? FechaResolucion { get; set; }
+        public String EstadoReporte { get; set; } //pendiente, en proceso, resuelto
+
+    }
+}

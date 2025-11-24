93bd7895e650483d15e3cdbeb3cd4f668203e6a7
Adan-Alvarado <alvaradoadan55@gmail.com>
2025-11-23 19:16:14 -0600

    Backup al antiguo program

--- Program.cs
+++ Program.cs
@@ -1,33 +1,16 @@
-using FixIt.Api.Services.Categoria;
-using FixIt.Entities;
-using Mapster;
-using MapsterMapper;
 using Microsoft.EntityFrameworkCore;
+using FixIt.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configurar DbContext SQLite
+// Agregar DbContext y la conexión
 builder.Services.AddDbContext<FixItDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Configurar Mapster
-var config = TypeAdapterConfig.GlobalSettings;
-// Ejemplo: config.NewConfig<CategoriaCreateDto, CategoriaEntity>().Map(dest => dest.Nombre, src => src.Nombre);
-
-builder.Services.AddMapster();
-builder.Services.AddScoped<IMapper, ServiceMapper>();
-
-// Registrar servicios
-builder.Services.AddScoped<CategoriaService>();
-
-// ---------------------------
-// Agregar controladores
 builder.Services.AddControllers();
 
-// Construir app
 var app = builder.Build();
 
-// Mapear rutas de los controladores
 app.MapControllers();
 
 app.Run();
